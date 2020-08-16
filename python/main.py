25832c8d594491a2d0c6c65f349c665684989fed
tanzaku <tanzaku@users.noreply.github.com>
2020-08-16 23:28:47 +0900

    変数名変更

--- python/main.py
+++ python/main.py
@@ -7,12 +7,12 @@ import torch.nn.functional as F
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = torch.nn.Linear(32, 16)
-        self.fc2 = torch.nn.Linear(16, 1)
+        self.linear1 = torch.nn.Linear(32, 16)
+        self.linear2 = torch.nn.Linear(16, 1)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = self.linear1(x)
+        x = self.linear2(x)
         return x
 
 
@@ -40,29 +40,29 @@ for i in range(N_EPOCHS):
 outputs = net(inputs)
 # print('outputs', outputs, 'loss', loss.item())
 
-with open('data/weight_row=16_col=32.bin', mode='wb') as fileobj:
-    # x = net.fc1.weight.to('cpu').detach().numpy().copy()
-    x = net.fc1.weight.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+with open('data/weight_row=16_col=32.bin', mode='wb') as f:
+    # x = net.linear1.weight.to('cpu').detach().numpy().copy()
+    x = net.linear1.weight.to('cpu').detach().numpy()
+    x.tofile(f)
 
-    x = net.fc1.bias.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+    x = net.linear1.bias.to('cpu').detach().numpy()
+    x.tofile(f)
 
-with open('data/weight_row=1_col=16.bin', mode='wb') as fileobj:
-    x = net.fc2.weight.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+with open('data/weight_row=1_col=16.bin', mode='wb') as f:
+    x = net.linear2.weight.to('cpu').detach().numpy()
+    x.tofile(f)
 
-    x = net.fc2.bias.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+    x = net.linear2.bias.to('cpu').detach().numpy()
+    x.tofile(f)
 
-with open('data/input.bin', mode='wb') as fileobj:
+with open('data/input.bin', mode='wb') as f:
     x = inputs.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+    x.tofile(f)
 
-with open('data/target.bin', mode='wb') as fileobj:
+with open('data/target.bin', mode='wb') as f:
     x = targets.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+    x.tofile(f)
 
-with open('data/output.bin', mode='wb') as fileobj:
+with open('data/output.bin', mode='wb') as f:
     x = outputs.to('cpu').detach().numpy()
-    x.tofile(fileobj)
+    x.tofile(f)
